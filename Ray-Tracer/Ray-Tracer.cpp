871f439de1d8aaf5a35a8e1ab48a9fbcba58299a
unknown <daimonas@abv.bg>
2021-05-25 16:08:02 +0300

    Added fuzziness parameter to the reflections.

--- Ray-Tracer/Ray-Tracer.cpp
+++ Ray-Tracer/Ray-Tracer.cpp
@@ -40,23 +40,25 @@ int main()
 {
 	//Image
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 800;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100;
 	const int max_depth = 50;
 	
 	// World
 	Hittable_list world;
+
 	auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
 	auto material_center = make_shared<Lambertian>(Color(0.7, 0.3, 0.3));
-	auto material_left = make_shared<Metal>(Color(0.8, 0.8, 0.8));
-	auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2));
+
+	//last parameter here is fuzziness which is how clear the sphere reflects
+	auto material_left = make_shared<Metal>(Color(0.9, 0.9, 0.9), 0.0);
+	auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.5);
 
 	world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<Sphere>(Point3(0.0, 0.0, -2.0), 0.5, material_center));
+	world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_center));
-	world.add(make_shared<Sphere>(Point3(0.0, 1.0, -0.80), 0.5, material_left));
+	world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
 	//Camera
 	Camera cam;
 	
